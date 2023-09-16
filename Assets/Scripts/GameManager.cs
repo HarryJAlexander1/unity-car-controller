9bdd1df8ea2b55723c3b3521de0025be32c59769
Harry Alexander <harryalexander1499@gmail.com>
2023-09-16 16:30:08 +0100

    import car prefab and create fundemental scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject PlayerPrefab;
+    public GameObject CameraPrefab;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPlayer();
+        SpawnCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnPlayer() 
+    {
+       Player = Instantiate(PlayerPrefab, new Vector3(0, 2, 0), Quaternion.identity);
+    }
+    private void SpawnCamera() 
+    {
+        Instantiate(CameraPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
